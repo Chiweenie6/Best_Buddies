3498231f56257a43b6e5de7684ee60db44fac423
chiweenie6 <113393706+Chiweenie6@users.noreply.github.com>
2023-03-22 11:19:46 -0700

    models created

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require("./Reaction");
+
+// Schema to create a Thought model
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: date => date.toLocaleDateString(),
+    },
+    username: {
+        type: String,
+        required: true,
+        ref: "User.username",
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property named "reactionCount" created to show number of reactions on a Thought
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+})
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create a User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        // Regex for an email validation
+        match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// Virtual property named "friendCount" created to show number of friends the User has
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+})
+
+const User = model('user', userSchema);
+
+module.exports = User;
